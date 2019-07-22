670bd9446d39d5f2c87ee153bed0a0297e005499
lixunhui <lixunhui@qq.com>
2019-07-22 18:11:58 +0800

    修改页面

--- utils/util.js
+++ utils/util.js
@@ -15,19 +15,19 @@ const formatNumber = n => {
 }
 
 // 自定义域名
- const host = 'https://wechat.eggworld.cn';
+//  const host = 'https://wechat.eggworld.cn';
 
-// const host = 'http://127.0.0.1';
-const appid ='wxdc9c6541158adc89';
-const meeting_name='第四届中国安全蛋品产业生态大会';
-const meeting_time='2019-05-04 ~ 2019-05-05';
-const meeting_location ='北京·黄河京都会议中心';
+const host = 'http://127.0.0.1';
+const appid ='wxf65b8972546a5d85';
+// const meeting_name='第四届中国安全蛋品产业生态大会';
+// const meeting_time='2019-05-04 ~ 2019-05-05';
+// const meeting_location ='北京·黄河京都会议中心';
 
 module.exports = {
   formatTime: formatTime,
   host:host,
-  appid:appid,
-  meeting_name: meeting_name,
-  meeting_time: meeting_time,
-  meeting_location: meeting_location
+  appid:appid
+  // meeting_name: meeting_name,
+  // meeting_time: meeting_time,
+  // meeting_location: meeting_location
 }
